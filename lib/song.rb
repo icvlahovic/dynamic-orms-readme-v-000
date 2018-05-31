c501a0037658e3e2dd7064e5f930fd76bdb5803c
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-31 19:16:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,9 +26,7 @@ class Song
   end
 
   def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
+    options.each { |property, value| self.send("#{property}=", value) }
   end
 
   def save
